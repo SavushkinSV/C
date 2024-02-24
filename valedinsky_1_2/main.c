d9d972ec117af50dd26b5d52a012b6206232b7a8
SavushkinSV <savushkin.sv@yandex.ru>
2024-02-24 14:05:22 +0300

    fix valedinsky_1_2

--- valedinsky_1_2/main.c
+++ valedinsky_1_2/main.c
@@ -1,6 +1,7 @@
 /*
 1.2
-Составьте программу для проверки является ли последовательность арифметической прогрессией.
+Составьте программу для проверки является ли последовательность арифметической
+прогрессией.
 */
 
 #include <stdio.h>
@@ -8,41 +9,41 @@
 void progression(FILE *fin);
 
 int main() {
-    char filename[256];
-    FILE *fin;
-    printf("Enter filename ->");
-    scanf("%s", filename);
-    fin = fopen(filename, "r");
-    if (!fin) {
-        printf("File not open %s\n", filename);
-        return -1;
-    }
+  char filename[256];
+  FILE *fin;
+  printf("Enter filename ->");
+  scanf("%s", filename);
+  fin = fopen(filename, "r");
+  if (!fin) {
+    printf("File not open %s\n", filename);
+    return -1;
+  }
 
-    progression(fin);
-    fclose(fin);
+  progression(fin);
+  fclose(fin);
 
-    return 0;
+  return 0;
 }
 
 void progression(FILE *fin) {
-    double x, y, d;
-    if (fscanf(fin, "%lf", &y) != 1) {
-        printf("There is not enough data in the file\n");
-        return;
-    }
+  double x, y, d;
+  if (fscanf(fin, "%lf", &y) != 1) {
+    printf("There is not enough data in the file\n");
+    return;
+  }
 
-    if (fscanf(fin, "%lf", &x) != 1) {
-        printf("There is not enough data in the file\n");
-        return;
-    }
+  if (fscanf(fin, "%lf", &x) != 1) {
+    printf("There is not enough data in the file\n");
+    return;
+  }
 
-    d = x - y;
-    while (fscanf(fin, "%lf", &y) == 1) {
-        if (y - x != d) {
-            printf("The sequence is not an arithmetic progression");
-            return;
-        }
-        x = y;
+  d = x - y;
+  while (fscanf(fin, "%lf", &y) == 1) {
+    if (y - x != d) {
+      printf("The sequence is not an arithmetic progression");
+      return;
     }
-    printf("The sequence is an arithmetic progression");
+    x = y;
+  }
+  printf("The sequence is an arithmetic progression");
 }
